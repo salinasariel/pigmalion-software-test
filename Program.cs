eb9413fc6ad792508b194f720f8e5e464642b8db
Ariel <93411684+salinasariel@users.noreply.github.com>
2025-03-02 23:10:54 -0300

    Final Exercice

--- Program.cs
+++ Program.cs
@@ -9,37 +9,34 @@ class Program
     {
         int[] nums = [1,4,3,9];
         //int[] nums = [1,2,4,4];
-
         int requiredSum = 8;
-        bool targetEjercicio1, targetEjercicio2;
+        
 
         
         Console.WriteLine(E1(nums, requiredSum));
         Console.WriteLine(E2(nums, requiredSum));
 
 
+        //bool targetEjercicio1, targetEjercicio2;
+        //Stopwatch watch = new();
+        //watch.Start();
+        //targetEjercicio1 = E1(nums, requiredSum);
+        //Console.WriteLine($"E1 {watch.ElapsedMilliseconds} ms. {targetEjercicio1}");
+        //watch.Stop();
 
-        Stopwatch watch = new();
-        watch.Start();
-        targetEjercicio1 = E1(nums, requiredSum);
-        Console.WriteLine($"E1 {watch.ElapsedMilliseconds} ms. {targetEjercicio1}");
-        watch.Stop();
+
+        //watch.Restart();
+        //targetEjercicio2 = E2(nums, requiredSum);
+        //Console.WriteLine($"E2: {watch.ElapsedMilliseconds} ms. {targetEjercicio2}");
+        //watch.Stop();
 
 
-        watch.Start();
-        targetEjercicio2 = E2(nums, requiredSum);
-        Console.WriteLine($"E2: {watch.ElapsedMilliseconds} ms. {targetEjercicio2}");
-        watch.Stop();
-
-        
 
     }
 
-    static bool E2(int[] nums, int requiredSum)
+    static bool E1(int[] nums, int requiredSum)
     {
-        bool target = false;
-
-        //Recorro el array tomando pibote i
+        //Recorro el array tomando pivote i
 
         for (int i = 0; i < nums.Length; i++)
         {
@@ -47,36 +44,38 @@ class Program
 
             for (int j = i + 1; j < nums.Length; j++)
             {
-                // Sumo el pibote i con j
+                // Sumo el pivote i con j
                 //Console.WriteLine(current + " + " + nums[j]+ "=" + (current + nums[j]));
                 if (current + nums[j] == requiredSum)
                 {
-                    target = true;
-                    return target;
+                    return true;
                 }
 
             }
             
         }
-        return target;
+        return false;
     }
 
-    static bool E1(int[] nums, int requiredSum)
+    static bool E2(int[] nums, int requiredSum)
     {
         //List vacio donde voy a ir almacenando de a uno los elementos del array 
-        List<int> exitstant = [];
+        List<int> existing = [];
 
         foreach (int i in nums) 
         {
-            // si la diferencia de requiredSum y el elemento, esta almacenado en mi list, devuelvo true
+            // si la diferencia de requiredSum y el elemento actual, esta almacenado en mi list, devuelvo true
             int diferent = requiredSum - i;
-            if (exitstant.Contains(diferent))
+            //Console.WriteLine(diferent);
+            if (existing.Contains(diferent))
             {
                 return true;
             }
-            exitstant.Add(i);
+            existing.Add(i);
         }
         return false;
     }
+
+
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # pigmalion-software-test
  Technical Test for Pigmalion company
+
+Test Tecnico para Pigmalion Software
+
+Dada la siguiente problemática: ¿puede un número X formarse
+usando la suma de 2 elementos de un array?
+Ejemplo 1
+Input: nums = [1,4,3,9], requiredSum = 8
+Output: False
+Ejemplo 2
+Input: nums = [1,2,4,4], requiredSum = 8
+Output: True
+Desarrolle (en pseudo código o su lenguaje de preferencia):
+1. Un algoritmo que resuelva el problema asumiendo que la máquina en donde va a correrse el
+programa tiene recursos infinitos, que el tiempo de ejecución no importa y que lo más
+importante es realizar el desarrollo en el menor tiempo posible.
+2. Un algoritmo que resuelva el problema asumiendo que los recursos son un bien preciado,
+que el tiempo de ejecución si importa y que el tiempo de desarrollo no es importante.
+Ante cualquier duda o ambigüedad en el enunciado, es libre de hacer todas las suposiciones
+necesarias, siempre y cuando las especifique.
+
+
+-- Interpretacion -- 
+Primer ejercicio:  Algoritmo sin restricciones de recuersos
+A - No importa los recursos utilizados.
+B - No importa el tiempo de ejecucion.
+C - Desarrollar el algoritmo en el menor tiempo posible.
+
+Segundo ejercicio: Algoritmo Optimizado
+A - Debo optimizar los recursos lo maximo posible.
+B - Debe ejecurtarse en el menor tiempo posible.
+C - No importe el tiempo de desarrollo del algoritmo.
